ace9ddc9ba232e0ecdde8bc21743dfd98a8e695b
MarcMunyozRuiz <152491243+MarcMunyozRuiz@users.noreply.github.com>
2025-09-25 20:55:27 +0200

    Update script.js

--- Ejercicios calentamiento/A qui toca ara/JS/script.js
+++ Ejercicios calentamiento/A qui toca ara/JS/script.js
@@ -2,16 +2,19 @@ let llista = [];
 
 function afegir() {
     let jugador = document.getElementById("name").value;
-
-    document.getElementById("jugadors").innerHTML += `${jugador}<br>`;
-    
-    llista.push(jugador)
+    llista.push(jugador);
+    pintarLlista();
+    //Creo una función para escribir la lista actualizada fácilmente
 }
 
 function seguent() {
-
-    document.getElementById("juga").innerHTML = llista[0]
+    document.getElementById("juga").innerHTML = llista[0];
     let first = llista.shift();
-    llista.push(first)
-
+    llista.push(first);
+    pintarLlista();
 }
+
+function pintarLlista(){
+    document.getElementById("jugadors").innerHTML = llista.join("<br>");
+    //Uso join para unir los elementos del array en un solo string (no me acuerdo si hemos usado esta nomenclaterura pero yo la aprendi haciendo mi TRB)
+}   
