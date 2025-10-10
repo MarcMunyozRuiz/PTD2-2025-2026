39dfaeba73a22bce0ebc4661b178bca5b53571a8
MarcMunyozRuiz <152491243+MarcMunyozRuiz@users.noreply.github.com>
2025-10-10 10:25:33 +0200

    Update script.js

--- Ejercicios calentamiento/Joc del penjat/JS/script.js
+++ Ejercicios calentamiento/Joc del penjat/JS/script.js
@@ -1,25 +1,30 @@
 let diccionario = ["JUEGO", "PROGRAMACION", "DESARROLLO", "JAVASCRIPT"];
+let palabra_adivinar = [];
 
+
+//Al incializar resetear la imagen y lista de letras tambien
 function Inicialitzar() {
     //Elegir palabra aleatoria del diccionario
-    let palabra_adivinar = diccionario[Math.floor(Math.random() * diccionario.length)];
-    //Convertir en array
-    Array.from(palabra_adivinar); 
+    palabra_adivinar = diccionario[Math.floor(Math.random() * diccionario.length)];
+    //Convertir en array divido por letras --> Spread operator
+    palabra_adivinar=[...palabra_adivinar];
     //Borrar la palabra para partida nueva
     document.getElementById("palabra").innerHTML = "";
+    alert(palabra_adivinar)
 }
 
 function Adivinar() {
     let letra = document.getElementById("letra").value.toUpperCase();
-    
-     for(let i = 0; i < letra.length; i++){
+    for(let i = 0; i < palabra_adivinar.length; i++){
 
-        if(palabra[i] === letra ){
+        if(palabra_adivinar[i] === letra ){
             //añadir a la lista de letras
+            alert("hello world")
             //añadir a la palabra para adivinar
         }
 
         else {
+            alert("no world :(")
             //cambiar imagen 
             //añadir a lista de letras
         }
@@ -31,3 +36,8 @@ function Adivinar() {
 //1 variable = palabra a adivinar ----> convertir en array
 //1 variable = numero de intentos
 //1 variable = letras falladas
+
+
+
+
+//Enseñar la palabra con guions ---> palabra_guiones []
